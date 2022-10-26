81db7276fa77631eb72e58614b9d08c71d0072cd
pheninux <58260568+pheninux@users.noreply.github.com>
2022-10-26 19:03:00 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 GOOS=linux GOARCH=amd64 go build ./cmd/web
 cp web agefice
 rm -f web
-scp agefice root@217.160.188.174:/var/www/go/deploy/agefice/
+scp agefice root@217.160.188.174:/var/www/go/deploy/agefice/        //old
 scp -r  '/mnt/c/Users/a706836/OneDrive - Worldline/Documents/GitHub/agefice/ui' root@217.160.188.174:/var/www/go/deploy/agefice/
 scp -r '/mnt/c/Users/a706836/OneDrive - Worldline/Documents/GitHub/agefice/tls' root@217.160.188.174:/var/www/go/deploy/agefice/
 
